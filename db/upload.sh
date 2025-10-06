383050267e7a7952125ef393419c5041bfcc1790
Dustin Sallings <dustin@spy.net>
2025-10-05 21:35:33 -1000

    Precompress CSVs

--- db/upload.sh
+++ db/upload.sh
@@ -11,6 +11,8 @@ consolidate() {
 
     duckdb -c "copy (select * from read_csv_auto('data_*.csv', header=true) order by $t) to 'data.csv' (format csv, header true)"
     rm data_*.csv
+    gzip -9v data.csv
+    mv data.csv.gz data.csv
 }
 
 find "$dwruns" -type f -name 'data_0.csv' -print0 |
@@ -18,11 +20,15 @@ find "$dwruns" -type f -name 'data_0.csv' -print0 |
         consolidate "$file" tsi
     done
 
-rclone sync $dwruns s3:db.downwind.pro/runs/  --progress
+rclone sync $dwruns s3:db.downwind.pro/runs/  --progress \
+    --header-upload "Content-Encoding: gzip" \
+    --header-upload "Content-Type: text/csv; charset=utf-8"
 
 find "$wind" -type f -name 'data_0.csv' -print0 |
     while IFS= read -r -d '' file; do
         consolidate "$file" ts
     done
 
-rclone sync $wind s3:db.downwind.pro/wind/  --progress
+rclone sync $wind s3:db.downwind.pro/wind/  --progress \
+    --header-upload "Content-Encoding: gzip" \
+    --header-upload "Content-Type: text/csv; charset=utf-8"
