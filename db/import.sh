e30754086b5fc46eb1dd123f706840a033e9dc2d
Dustin Sallings <dustin@spy.net>
2026-07-25 07:30:49 -1000

    Auto-archive processed files

--- db/import.sh
+++ db/import.sh
@@ -5,6 +5,7 @@ lake=$HOME/stuff/duck
 srcdir='/Users/dustin/Library/Mobile Documents/iCloud~TNT~Waterspeed/Documents/runs'
 
 mv "$srcdir/"Waterspeed-List* $HOME/Downloads/list.csv
+archive='/Volumes/dustin/data/waterspeed'
 
 h=`pwd`
 importlist=`pwd`/import-list.sql
@@ -14,6 +15,11 @@ echo "List import"
 duckdb --init init.sql < $importlist
 echo "Individual import"
 duckdb --init init.sql < $import
+
+if [ -d "$archive" ]
+then
+    mv "$srcdir/"*.csv "$archive"
+fi
 
 cd "$h"
 ./upload.sh
