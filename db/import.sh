da438f60482f7909dc786190d9b811d8d7148d33
Dustin Sallings <dustin@spy.net>
2026-09-19 07:29:24 -1000

    Updated my import thing

--- db/import.sh
+++ db/import.sh
@@ -2,24 +2,12 @@
 
 
 lake=$HOME/stuff/duck
-srcdir='/Users/dustin/Library/Mobile Documents/iCloud~TNT~Waterspeed/Documents/runs'
-
-mv "$srcdir/"Waterspeed-List* $HOME/Downloads/list.csv
-archive='/Volumes/dustin/data/waterspeed'
 
 h=`pwd`
-importlist=`pwd`/import-list.sql
 import=`pwd`/import.sql
 cd $lake
-echo "List import"
-duckdb --init init.sql < $importlist
-echo "Individual import"
+echo "Full import"
 duckdb --init init.sql < $import
 
-if [ -d "$archive" ]
-then
-    mv "$srcdir/"*.csv "$archive"
-fi
-
 cd "$h"
-./upload.sh
+./upload-runs.sh
